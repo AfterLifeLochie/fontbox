02bfcc7d135a923d71a738d2cae9b50a8a0cb4f9
AfterLifeLochie <afterlifelochie@afterlifelochie.net>
2014-09-11 18:01:49 +1000

    Adding master font rendering code.

---
+++ src/main/java/net/afterlifelochie/fontbox/FontException.java
@@ -0,0 +1,13 @@
+package net.afterlifelochie.fontbox;
+
+public class FontException extends Exception {
+
+	public FontException(String reason) {
+		super(reason);
+	}
+
+	public FontException(String reason, Throwable cause) {
+		super(reason, cause);
+	}
+
+}
