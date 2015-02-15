42180c91d002eab3eb63a59d57a48124145629ac
Lochie <afterlifelochie@afterlifelochie.net>
2015-02-15 14:38:43 +1100

    Renderbuffers need a rewrite... yuck.

--- src/tests/java/net/afterlifelochie/demo/FontboxClient.java
+++ src/tests/java/net/afterlifelochie/demo/FontboxClient.java
@@ -6,14 +6,12 @@ import net.afterlifelochie.fontbox.Fontbox;
 import net.afterlifelochie.fontbox.GLFont;
 import net.afterlifelochie.fontbox.api.PrintOutputTracer;
 import net.afterlifelochie.fontbox.layout.LayoutCalculator;
-import net.afterlifelochie.fontbox.render.FontRenderBuffer;
 import net.afterlifelochie.fontbox.render.WrittenFontRenderer;
 import net.minecraft.util.ResourceLocation;
 
 public class FontboxClient extends FontboxServer {
 
 	public GLFont daniel, notethis, ampersand;
-	public FontRenderBuffer fontBuffer;
 	public LayoutCalculator fontCalculator;
 	public WrittenFontRenderer renderer;
 
@@ -25,8 +23,7 @@ public class FontboxClient extends FontboxServer {
 			daniel = GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/daniel.ttf"));
 			notethis = GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/notethis.ttf"));
 			ampersand = GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/ampersand.ttf"));
-			
-			fontBuffer = FontRenderBuffer.fromFont(daniel);
+
 			fontCalculator = new LayoutCalculator();
 			renderer = new WrittenFontRenderer();
 		} catch (FontException f0) {
