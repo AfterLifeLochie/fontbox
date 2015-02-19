74cd7ae855dc0d6d5475a88b34c6d8735e971011
Lochie <afterlifelochie@afterlifelochie.net>
2015-02-19 21:37:17 +1100

    Enable CompilerHint to store multiple hint type flags

--- src/main/java/net/afterlifelochie/fontbox/document/CompilerHint.java
+++ src/main/java/net/afterlifelochie/fontbox/document/CompilerHint.java
@@ -1,14 +1,20 @@
 package net.afterlifelochie.fontbox.document;
 
+import java.util.EnumSet;
+
 public class CompilerHint extends Element {
 
 	public static enum HintType {
 		PAGEBREAK, FLOATBREAK;
 	}
 
-	public HintType type;
+	public EnumSet<HintType> types;
 
 	public CompilerHint(HintType type) {
-		this.type = type;
+		this.types = EnumSet.of(type);
+	}
+	
+	public CompilerHint(EnumSet<HintType> types) {
+		this.types = types;
 	}
 }
