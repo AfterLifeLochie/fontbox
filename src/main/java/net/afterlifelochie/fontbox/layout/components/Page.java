bd9106d37d37b4fc0ae0754233da517fce3e57f9
Lochie <afterlifelochie@afterlifelochie.net>
2015-02-22 21:29:18 +1100

    Add Page in bounds functions. Add to tests.

--- src/main/java/net/afterlifelochie/fontbox/layout/components/Page.java
+++ src/main/java/net/afterlifelochie/fontbox/layout/components/Page.java
@@ -3,6 +3,7 @@ package net.afterlifelochie.fontbox.layout.components;
 import java.util.ArrayList;
 
 import net.afterlifelochie.fontbox.document.Element;
+import net.afterlifelochie.fontbox.layout.ObjectBounds;
 
 /**
  * One whole page containing a collection of spaced lines with line-heights and
@@ -27,5 +28,38 @@ public class Page extends Container {
 	public Page(PageProperties properties) {
 		super(properties.width, properties.height);
 		this.properties = properties;
+	}
+
+	/**
+	 * Determine if the provided bounding box intersects with an existing
+	 * element on the page. Returns true if an intersection occurs, false if
+	 * not.
+	 * 
+	 * @param bounds
+	 *            The bounding box to check
+	 * @return If an intersection occurs
+	 */
+	public boolean intersectsElement(ObjectBounds bounds) {
+		for (Element element : elements)
+			if (element.bounds() != null)
+				if (element.bounds().intersects(bounds))
+					return true;
+		return false;
+	}
+
+	/**
+	 * Determine if the provided bounding box fits entirely on the page. Returns
+	 * true if the bounding box fits inside the page, false if not.
+	 * 
+	 * @param bounds
+	 *            The bounding box to check
+	 * @return If the bounding box fits inside the page
+	 */
+	public boolean insidePage(ObjectBounds bounds) {
+		if (bounds.x < 0 || bounds.y < 0 || bounds.x > width || bounds.y > height)
+			return false;
+		if (bounds.x + bounds.width > width || bounds.y + bounds.height > height)
+			return false;
+		return true;
 	}
 }
--- src/tests/java/fontbox/ReliabilityTests.java
+++ src/tests/java/fontbox/ReliabilityTests.java
@@ -7,23 +7,44 @@ import java.util.EnumSet;
 import net.afterlifelochie.fontbox.document.CompilerHint;
 import net.afterlifelochie.fontbox.document.CompilerHint.HintType;
 import net.afterlifelochie.fontbox.layout.ObjectBounds;
+import net.afterlifelochie.fontbox.layout.components.Page;
+import net.afterlifelochie.fontbox.layout.components.PageProperties;
 
 import org.junit.Test;
 
 public class ReliabilityTests {
 
+	/**
+	 * Test to check if the ObjectBounds intersections are computed correctly.
+	 */
 	@Test
 	public void testObjectBoundsIntersects() {
 		ObjectBounds box0 = new ObjectBounds(30, 30, 30, 30, false);
 		ObjectBounds box1 = new ObjectBounds(45, 45, 15, 15, false);
 		ObjectBounds box2 = new ObjectBounds(100, 100, 20, 20, false);
-
 		assertTrue("box0 intersects box1", box0.intersects(box1));
 		assertTrue("box1 intersects box0", box1.intersects(box0));
 		assertFalse("box2 !intersects box1", box2.intersects(box1));
 		assertFalse("box1 !intersects box2", box1.intersects(box2));
 	}
 
+	
+	/**
+	 * Test to check that Page correctly computes element bounds.
+	 */
+	@Test
+	public void testPageBounding() {
+		Page testPage = new Page(new PageProperties(100, 100));
+		assertTrue("box is inside page", testPage.insidePage(new ObjectBounds(0, 0, 100, 100, false)));
+		assertTrue("box is inside page", testPage.insidePage(new ObjectBounds(25, 25, 50, 50, false)));
+		assertFalse("box is outside bounds", testPage.insidePage(new ObjectBounds(-20, -20, 140, 140, false)));
+		assertFalse("box is outside bounds", testPage.insidePage(new ObjectBounds(50, 50, 100, 100, false)));
+	}
+
+	/**
+	 * Test to check that CompilerHints doesn't accept bad constructor
+	 * parameters.
+	 */
 	@Test
 	public void testCompilerHints() {
 		try {
