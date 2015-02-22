4b9cc81452f6ca0cae98017698ce442ca8e14440
Lochie <afterlifelochie@afterlifelochie.net>
2015-02-22 22:01:50 +1100

    Add GUI stub

---
+++ src/main/java/net/afterlifelochie/fontbox/render/BookGUI.java
@@ -0,0 +1,35 @@
+package net.afterlifelochie.fontbox.render;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class BookGUI extends GuiScreen {
+
+	@Override
+	public void initGui() {
+	}
+
+	@Override
+	public void updateScreen() {
+	}
+
+	@Override
+	public void drawScreen(int mx, int my, float frames) {
+	}
+
+	@Override
+	protected void keyTyped(char val, int code) {
+	}
+
+	@Override
+	protected void mouseClicked(int mx, int my, int button) {
+	}
+
+	@Override
+	protected void mouseMovedOrUp(int mx, int my, int button) {
+	}
+
+	@Override
+	protected void mouseClickMove(int mx, int my, int button, long ticks) {
+	}
+
+}
