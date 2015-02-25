af2a424b58646a5917ccd9bf9dabbdac54781bc1
Lochie <afterlifelochie@afterlifelochie.net>
2015-02-25 15:01:57 +1100

    Cleanup demo code in clientproxy

--- src/demo/java/net/afterlifelochie/demo/FontboxClient.java
+++ src/demo/java/net/afterlifelochie/demo/FontboxClient.java
@@ -9,16 +9,14 @@ import net.minecraft.util.ResourceLocation;
 
 public class FontboxClient extends FontboxServer {
 
-	public GLFont daniel, notethis, ampersand;
-
 	@Override
 	public void init(FMLInitializationEvent e) {
 		super.init(e);
 		try {
 			Fontbox.setTracer(new PrintOutputTracer());
-			daniel = GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/daniel.ttf"));
-			notethis = GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/notethis.ttf"));
-			ampersand = GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/ampersand.ttf"));
+			GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/daniel.ttf"));
+			GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/notethis.ttf"));
+			GLFont.fromTTF(Fontbox.tracer(), new ResourceLocation("fontbox", "fonts/ampersand.ttf"));
 		} catch (FontException f0) {
 			f0.printStackTrace();
 		}
