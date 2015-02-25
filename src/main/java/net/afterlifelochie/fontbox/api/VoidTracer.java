41efed40432563c3ea8ad425f30a35b9a6db47ac
Lochie <afterlifelochie@afterlifelochie.net>
2015-02-25 15:12:18 +1100

    Update tracers to allow for warn(varargs). Void tracer doesn't ignore warnings, though.

--- src/main/java/net/afterlifelochie/fontbox/api/VoidTracer.java
+++ src/main/java/net/afterlifelochie/fontbox/api/VoidTracer.java
@@ -1,8 +1,10 @@
 package net.afterlifelochie.fontbox.api;
 
 /**
+ * <p>
  * Void tracer. Does exactly that - void. You should use this in place of
  * passing null to the tracer arguments in method calls.
+ * </p>
  * 
  * @author AfterLifeLochie
  *
@@ -12,6 +14,16 @@ public class VoidTracer implements ITracer {
 	@Override
 	public void trace(Object... params) {
 		/* Do nothing */
+	}
+
+	@Override
+	public void warn(Object... params) {
+		Object[] data = params;
+		StringBuilder result = new StringBuilder();
+		for (int i = 0; i < data.length; i++)
+			result.append(data[i]).append(", ");
+		String r0 = result.toString();
+		System.out.println("Fontbox warning: " + r0.substring(0, r0.length() - 2));
 	}
 
 }
