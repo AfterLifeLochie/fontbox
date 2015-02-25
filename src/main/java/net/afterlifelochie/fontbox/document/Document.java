f38288ee830b2cc6c798e13be282c62cee856588
Lochie <afterlifelochie@afterlifelochie.net>
2015-02-25 14:55:43 +1100

    Add Document storage methods.

--- src/main/java/net/afterlifelochie/fontbox/document/Document.java
+++ src/main/java/net/afterlifelochie/fontbox/document/Document.java
@@ -10,4 +10,22 @@ public class Document {
 		this.elements = new ArrayList<Element>();
 	}
 
+	public void push(Element element) {
+		if (elements.contains(element))
+			throw new IllegalArgumentException("Element already exists in tree!");
+		elements.add(element);
+	}
+
+	public Element pop() {
+		return elements.remove(elements.size() - 1);
+	}
+
+	public Element head() {
+		return elements.get(0);
+	}
+
+	public Element tail() {
+		return elements.get(elements.size() - 1);
+	}
+
 }
